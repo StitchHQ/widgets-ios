8827b4497b15045daf4e26d11311ece2740d7068
sudarvizhi <vizhi1930@gmail.com>
2025-03-06 22:13:10 +0530

    jail break

--- StitchWidget/StitchWidget/Widget/Common/Extension/ExtensionButton.swift
+++ StitchWidget/StitchWidget/Widget/Common/Extension/ExtensionButton.swift
@@ -21,3 +21,26 @@ extension UIButton {
         self.layer.masksToBounds = true
     }
 }
+/**
+     Detect that the app is running on a jailbroken device or not
+
+     - returns: bool value for jailbroken device or not
+     */
+public func isDeviceJailbroken() -> Bool {
+        #if arch(i386) || arch(x86_64)
+            return false
+        #else
+            let fileManager = FileManager.default
+
+            if (fileManager.fileExists(atPath: "/bin/bash") ||
+                fileManager.fileExists(atPath: "/usr/sbin/sshd") ||
+                fileManager.fileExists(atPath: "/etc/apt") ||
+                fileManager.fileExists(atPath: "/private/var/lib/apt/") ||
+                fileManager.fileExists(atPath: "/Applications/Cydia.app") ||
+                fileManager.fileExists(atPath: "/Library/MobileSubstrate/MobileSubstrate.dylib")) {
+                return true
+            } else {
+                return false
+            }
+        #endif
+    }
